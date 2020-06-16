0053040396e1ed2e0cdd85635e94f20caa7e0a5a
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-16 18:48:31 -0500

    api key works

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
 
+function displayLocationCurrentWeather(){
+
+// var weatherLocation = 
+// var queryURL = "https://:api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid={9ac0eaae4cb7573a493c5ffeea19100f}";
+
+// var currentCity = $(this)
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName + "&appid=9ac0eaae4cb7573a493c5ffeea19100f";
 
 
+$.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response) {
+    console.log(response);
+  });
 
-
+}
+displayLocationCurrentWeather();
 //five day forcast in a loop
 
 
