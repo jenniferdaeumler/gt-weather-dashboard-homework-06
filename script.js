6418e4ade073a6d0a3b77986c243590371efdec7
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-16 19:03:05 -0500

    current date with moment in col

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-$(document).ready(function(){
-
-function displayLocationCurrentWeather(){
-
-// var weatherLocation = 
-// var queryURL = "https://:api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid={9ac0eaae4cb7573a493c5ffeea19100f}";
-
-// var currentCity = $(this)
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName + "&appid=9ac0eaae4cb7573a493c5ffeea19100f";
+$(document).ready(function () {
+    
+var currentDate = moment().format("L");
+$("#current-city").append(currentDate);
+//   function displayLocationCurrentWeather() {
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function(response) {
-    console.log(response);
-  });
+//     var queryURL =
+//       "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=9ac0eaae4cb7573a493c5ffeea19100f";
 
-}
-displayLocationCurrentWeather();
-//five day forcast in a loop
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET",
+//     }).then(function (response) {
+//       console.log(response);
+//     });
 
 
 
 
 
-
-
-
+//     // //Search for city, display on screen
+//     // $("search-button").on("click", function (event) {
+//     //   event.preventDefault();
+//     //   // This line of code will grab the input from the textbox
+//     //   var cityName = $("#search-input").val().trim();
+//     //   console.log(cityName);
+//     // });
+//   }
+//   displayLocationCurrentWeather();
+  //five day forcast in a loop
 });
