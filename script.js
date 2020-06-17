fde1bcf498b9edc0b99f78e578fe5c6b9ee9bcea
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-17 07:19:03 -0500

    temp/windspeed/humidity for searched city

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
 $(document).ready(function () {
-  var currentDate = moment().format("L");
-  var apiKey = "1935f5d7d75a269680ddfadd7b264dcb";
-  var searchInput = $("#search-input").val().trim();
-//   var tempEl = response.main.temp;
-//   var humidityEl = response.main.humidity;
-//   var windSpeedEl = response.wind.speed;
-  var previouslySearchedCities = localStorage.getItem(
-    $("#search-input").val().trim()
-  );
-  $("#previous-search").append(previouslySearchedCities);
-
-  $("#search-button").on("click", function (event) {
-    $("#current-city").empty();
-    event.preventDefault();
-    var textValue = $(this).siblings("#search-input").val();
-    var textKey = "City Searched";
-    console.log(textKey, textValue);
-    localStorage.setItem(textKey, textValue);
-    // This line of code will grab the input from the textbox
+    var currentDate = moment().format("L");
+    var apiKey = "1935f5d7d75a269680ddfadd7b264dcb";
     var searchInput = $("#search-input").val().trim();
-    $("#current-city")
-      .append(searchInput)
-      .append(" " + currentDate);
-    // $("#previous-search").addClass("<li>");
-    // $("#previous-search").append("#search-input");
-    var queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&units=imperial&appid=" +
-      apiKey;
+    var previouslySearchedCities = localStorage.getItem(
+        $("#search-input").val().trim()
+    );
+    $("#previous-search").append(previouslySearchedCities);
 
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-    //   console.log(response);
-      var tempEl = (response.main.temp);
-      $("#temperature").text("Temperature: " + tempEl);
+    $("#search-button").on("click", function (event) {
+        $("#current-city").empty();
+        event.preventDefault();
+        var textValue = $(this).siblings("#search-input").val();
+        var textKey = "City Searched";
+        console.log(textKey, textValue);
+        localStorage.setItem(textKey, textValue);
+        // This line of code will grab the input from the textbox
+        var searchInput = $("#search-input").val().trim();
+        $("#current-city")
+            .append(searchInput)
+            .append(" " + currentDate);
+        // $("#previous-search").addClass("<li>");
+        // $("#previous-search").append("#search-input");
+        var queryURL =
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            searchInput +
+            "&units=imperial&appid=" +
+            apiKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            var tempEl = response.main.temp;
+            var humidityEl = response.main.humidity;
+            var windSpeedEl = response.wind.speed;
+
+            $("#temperature").text("Temperature: " + Math.round(tempEl) + String.fromCharCode(176) + "F");
+            $("#humidity").text("Humidity: " + humidityEl + "%");
+            $("#wind-speed").text("Wind Speed: " + windSpeedEl + " MPH");
+        });
+        //five day forcast in a loop
     });
-    //five day forcast in a loop
-  });
-
-  
 });
